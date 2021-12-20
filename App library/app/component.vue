fd7cedb9277f1519e9b767b94a4c5616cc1a58a9
Daniel Trương <khicononline9xtq@icloud.com>
2021-12-20 17:04:26 +0700

    App lib

---
+++ App library/app/component.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <h3 v-html="data?.settings?.title"></h3>
+        <p v-html="data?.settings?.text">
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: "The app",
+    props: {
+        data: {
+            type: Object,
+            default() {
+                return {};
+            }
+        }
+    },
+    computed: {
+        settings(){
+            return [
+                {
+                    type: 'text',
+                    name:'title',
+                    label: 'title'
+                },
+                {
+                    type: 'textarea',
+                    name:'text',
+                    label: 'Text'
+                }
+            ]
+        },
+        
+        javascript(){
+            return function(){
+                const $el = this.$el;
+             
+                console.log($el);
+
+
+            }
+        }
+    },
+    mounted() {
+        if(!this.data?.settings)
+        {
+            this.data.settings = {
+                title: 'Ai cha cha',
+                text: 'Lorem ispum'
+            }
+        }
+    },
+}
+</script>
